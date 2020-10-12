d9581ab2dc3bd8072f92260e6095a13c86d60fe7
bug <young@bug>
2020-10-12 20:00:07 +0800

    V0.2 - 基本完成顺序线性表代码

--- StudyForDataStructure/StudyForDataStructure/SeqList.h
+++ StudyForDataStructure/StudyForDataStructure/SeqList.h
@@ -18,16 +18,13 @@ public:
 	SeqList() { Last = 0; };//无参构造函数
 	SeqList(const elemType array[], const int &n)//有参构造函数
 	{
-		if (n > MAXSIZE) { cout << "The array is so large!" << endl; }
-		else {
-			for (int i = 0; i < n; i++) {
-				Data[i] = array[i];
-			}
-			Last = n;
+		for (int i = 0; i < n; i++) {
+			Data[i] = array[i];
 		}
+		Last = n;
 	}
 	~SeqList() {};//析构函数
-
+	
 	Position findByValue(elemType x)//根据有序列，查找某个元素出现的第一个位置，并返回他的位置
 	{
 		{
@@ -38,29 +35,10 @@ public:
 			else return i;
 		}
 	}
-	void insert(elemType x, Position index) {
-		if (Last == MAXSIZE - 1) { cout << "the array is full！" << endl; return; }
-		if (index < 0 || index>(Last + 2)) { cout << "the array index is fault!" << endl; return; }
-		for (int end = Last; end >= index - 1; end--) {
-			Data[end + 1] = Data[end];//倒着循环，把最后一个值赋给他的下一个
-		}
-		Data[index - 1] = x;//index-1 是因为从0 开始
-		Last++;//更新长度
-		return;
-	}
-	void deleteByIndex(Position index) {
-		if (Last == MAXSIZE - 1) { cout << "the array is full！" << endl; return; }
-		if (index < 0 || index>(Last + 2)) { cout << "the array index is fault!" << endl; return; }
-		for (; index < Last; index++) {
-			Data[index - 1] = Data[index];
-		}
-		Last--;
-		return;
-	}
 	void showLength() { cout << "length = " << Last << endl; }
 	void showData() {
 		cout << "show data: " << endl;
-		for (Position i = 0; i < Last; i++) {
+		for (Position i = 0; i < Last;i++) {
 			cout << Data[i] << endl;
 		}
 	}
@@ -73,11 +51,11 @@ private:
 template <class elemType>
 SeqList<elemType>::SeqList(const elemType array[], const int &n)//有参构造函数
 {// SeqList<elemType>::SeqList(const elemType array[], const int &n)
-// 这样写会出现link2019 的错误，原因是声明和引用不匹配？
-for (int i = 0; i < n; i++) {
-Data[i] = array[i];
-}
-Last = n;
+ // 这样写会出现link2019 的错误，原因是声明和引用不匹配？
+	for (int i = 0; i < n; i++) {
+		Data[i] = array[i];
+	}
+	Last = n;
 }
 
 
@@ -85,11 +63,11 @@ Last = n;
 template <class elemType>
 Position SeqList<elemType>::findByValue(elemType x)
 {
-Position i = 0;
-while (i <= Last && Data[i] != x)
-i++;
-if (i > Last) return ERROR;
-else return i;
+	Position i = 0;
+	while (i <= Last && Data[i] != x)
+		i++;
+	if (i > Last) return ERROR;
+	else return i;
 }
 */
 
